1abb604f5ee6f3082b084f78b1b3228041f8d833
jeansung <jsung@hmc.edu>
2015-07-17 18:08:37 -0700

    Rebuilding site Fri Jul 17 18:08:37 PDT 2015

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Black        0;30     Dark Gray     1;30
+# Red          0;31     Light Red     1;31
+# Green        0;32     Light Green   1;32
+# Brown/Orange 0;33     Yellow        1;33
+# Blue         0;34     Light Blue    1;34
+# Purple       0;35     Light Purple  1;35
+# Cyan         0;36     Light Cyan    1;36
+# Light Gray   0;37     White         1;37
+
+echo -e "${CYAN}Deploying updates to GitHub...${NC}"
+CYAN='\033[0;36m'
+NC='\033[0m'
+
+# Build the project. 
+hugo --theme material-design
+
+# Add changes to git.
+git add -A
+
+# Commit changes.
+msg="Rebuilding site `date`"
+if [ $# -eq 1 ]
+  then msg="$1"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
+git subtree push --prefix=public git@github.com:jeansung/blog.git gh-pages
